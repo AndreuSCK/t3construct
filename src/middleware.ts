8a8814289044382ce4b5bd6d4ba273c4f79ad87f
AndreuSCK <andreu.sck@gmail.com>
2024-03-27 22:58:25 +0100

    Add svix package and dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  publicRoutes: ['/',],
+  publicRoutes: ["/", "/api/webhooks(.*)"],
 
   // Redirect signed out users to the sign in page:
   // signInRoute: '/sign-in',
@@ -13,13 +13,13 @@ export default authMiddleware({
   // ignoredRoutes:["/api/(.*)"]
   // ignoredRoutes:[""]
 });
- 
+
 export const config = {
   matcher: [
     // Exclude files with a "." followed by an extension, which are typically static files.
     // Exclude files in the _next directory, which are Next.js internals.
     "/((?!.+\\.[\\w]+$|_next).*)",
     // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+    "/(api|trpc)(.*)",
+  ],
 };
